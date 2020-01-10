afbb5f30d35ae5c60473e9b3657a4745e9cfb2af
WaelAbuaishahNabed <wael.abuaishah@nabed.net>
2020-01-10 19:54:47 +0200

    testing format

--- app/src/main/java/com/example/choosecolorapp/MainActivity.kt
+++ app/src/main/java/com/example/choosecolorapp/MainActivity.kt
@@ -1,12 +1,21 @@
 package com.example.choosecolorapp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        if (true)
+            1
+        else
+            0
+
+        try {
+        } catch (e: Exception) {
+        }
     }
 }
